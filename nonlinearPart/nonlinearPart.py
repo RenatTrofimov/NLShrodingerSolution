865f8cdc2dbd69c8bd986beb193184c45bfe0ec7
Renat <renatrofimov@gmail.com>
2024-08-16 16:41:25 +0300

    r160824

--- nonlinearPart/nonlinearPart.py
+++ nonlinearPart/nonlinearPart.py
@@ -9,10 +9,8 @@ def alpha_sq(s,q):
 
 def b_sq(s,q):
     tempeture = 1.0
-    gamma_0 = 1.0
-    q_s = np.arange(1,10, dtype=float64)
-    return -q * (alpha_sq(s,q)/gamma_0) * (integrate.quad(lambda r: cos(q*r)*exp(-np.sum(cos(q_s*r)*alpha_sq(s,q_s))/tempeture), -pi, pi)) / (integrate.quad(lambda r: exp(-np.sum(cos(q_s*r)*alpha_sq(s,q_s))/tempeture), -pi, pi))
+    gamma_0 = 2.7
+    q_s = np.arange(1,10)
+    return -q * (alpha_sq(s,q)/gamma_0) * (integrate.quad(lambda r: cos(q*r)*exp(-np.sum(cos(q_s*r)*alpha_sq(s,q_s))/tempeture), -pi, pi)) #/ (integrate.quad(lambda r: exp(-np.sum(cos(q_s*r)*alpha_sq(s,q_s))/tempeture), -pi, pi))
     
-
-result = integrate.quad(lambda x: sin(x), 0, 4.5)
-print (result)
+print (b_sq(1.,1.))
