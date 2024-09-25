4ad88adbc46aac9f7ad30617a56e38f49b183bee
unknown <renatrofimov@gmail.com>
2024-09-25 12:42:29 +0300

    set coefficients

--- main.py
+++ main.py
@@ -1,37 +1,49 @@
 from TM.thomasMethod import thomas
 from nonlinearPart.nonlinearPart import get
+from matplotlib import pyplot as plt
 import numpy as np
 # Main Program
 #step 1: Create Coefficient and LHS matrices store (a,b,c,d) seperately
 len = 5
 
-hx = 0.1
-ht = 0.1
+hx = 0.06
+ht = 0.01
 x = np.arange(0,6,hx)
 Xc = np.exp(-(x-3)**2)
 Xn = np.insert(Xc[:-1], 0, 0)
 Xp = np.append(Xc[1:], 0)
+plt.plot(x, np.abs(Xc))
+plt.show()
+C = np.fromiter((get(_x) for _x in Xc), Xc.dtype, count=Xc.shape[0])
+w0 = np.complex64(1e14) 
+w = np.complex64(5e14) 
+eps = np.complex64(4.0) 
+A = np.complex64(1.0)
+
+B = 2.*1j*w*np.sqrt(eps)/w0
 
 
-z = np.complex128(0.1)
-y = np.complex128(0.1)
-C = Xc*0.1
-c0 = np.complex128(0.1)
-w0 = np.complex128(0.1) 
-A = np.complex128(1.0)
-k = np.complex128(1.0)
-B = 2.*1j*k*c0/w0
-D = Xc*0.1
+a0 = np.ones(Xc.shape[0], dtype = np.complex64)
+b0 = np.ones(Xc.shape[0], dtype = np.complex64)
+c0 = np.ones(Xc.shape[0], dtype = np.complex64)
 
-a = (0.5*A/hx**2)*np.ones(Xc.shape[0], dtype = np.complex128)
-b = (B/ht - A/hx**2)*np.ones(Xc.shape[0], dtype = np.complex128)
-c = (0.5*A/hx**2)*np.ones(Xc.shape[0], dtype = np.complex128)
-d = Xc*(B/ht + A/hx**2) + C - 0.5*A*(Xn - Xp)
+a = (0.5*A/hx**2)*a0
+b = (B/ht - A/hx**2)*b0
+c = (0.5*A/hx**2)*c0
+d = Xc*(B/ht + A/hx**2) - C*Xc - 0.5*A*(Xn + Xp)
 
-# Call thomas function
-X = thomas(a,b,c,d, np.dtype(np.complex128))
-# Print x
-print('The values of x are: ',x)
-from matplotlib import pyplot as plt
-plt.plot(np.abs(X))
+X = thomas(a,b,c,d, np.dtype(np.complex64))
+print("run")
+for i in range(10):
+    
+    C = np.fromiter((get(_x) for _x in X), X.dtype, count=Xc.shape[0])
+    Xn = np.insert(X[:-1], 0, 0)
+    Xp = np.append(X[1:], 0)
+    
+    d = X*(B/ht + A/hx**2) - C*X - 0.5*A*(Xn + Xp)
+    
+    X = thomas(a,b,c,d, np.dtype(np.complex64))
+
+plt.plot(x, np.abs(X))
+plt.plot(x, np.abs(Xc))
 plt.show()
