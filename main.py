026479f17b568b02faea99012601e1c5e9f3854d
Renat Trofimov <renatrofimov@gmail.com>
2025-02-02 23:56:15 +0300

    res020225

--- main.py
+++ main.py
@@ -1,16 +1,18 @@
 from TM.thomasMethod import thomas
-from nonlinearPart.nonlinearPart import get
+from nonlinearPart.nonlinearPart import NonlinearPart as NNP
 from matplotlib import pyplot as plt
 from scipy.linalg import solve_banded
 from joblib import Parallel, delayed
+from numba import njit
 import numpy as np
 import multiprocessing
-
+_NNP = NNP()
 def execute(values):
+	global NNP 
 	global nnl, Xc
 	for i in range(values[0],values[1]):
-		nnl[i] = get(Xc[i])
-		print(i)
+		nnl[i] = NNP.get(Xc[i])
+		print(nnl[i])
 
 
 def process(i):
@@ -18,13 +20,14 @@ def process(i):
 
 # Main Program
 #step 1: Create Coefficient and LHS matrices store (a,b,c,d) seperately
-hx = 0.001
+hx = 0.01
 ht = 0.001
 x = np.arange(0,5,hx, dtype=np.complex128)
 Xc = np.exp(-1*(x-2)**2)*np.exp(1j*(x-2))
 X0 = np.exp(-1*(x-2)**2)*np.exp(1j*(x-2))
 Xn = np.insert(Xc[:-1], 0, 0)
 Xp = np.append(Xc[1:], 0)
+##################################################################################
 a = np.zeros(x.shape[0], dtype=np.complex128)
 c = np.zeros(x.shape[0], dtype=np.complex128)
 a[2:x.shape[0]-1] = a[2:x.shape[0]-1] + 1/(2*hx**2)
@@ -33,21 +36,23 @@ ab = np.ones(x.shape[0], dtype=np.complex128)
 ab[1:-1] = ab[1:-1]*(10*2j/hx - hx**(-2))
 ab = np.array([a, ab, c], dtype=np.complex128)
 nnl = np.zeros(x.shape[0])
+###################################################################################
 pool_obj = multiprocessing.Pool()
-pool_indexes = np.zeros((16,2), dtype=np.int32)
-for index in range(16):
-	pool_indexes[index][0] = int(x.shape[0]/16)*index
-	pool_indexes[index][1] = int(x.shape[0]/16)*(index + 1) - 1
+pool_indexes = np.zeros((256,2), dtype=np.int32)
+for index in range(256):
+	pool_indexes[index][0] = int(x.shape[0]/256)*index
+	pool_indexes[index][1] = int(x.shape[0]/256)*(index + 1) - 1
 pool_indexes[0][0] = 0
-pool_indexes[15][1] = x.shape[0] - 1
+pool_indexes[255][1] = x.shape[0] - 1
 print(pool_indexes)
+###################################################################################
 for j in range(10):
-	for i in range(100):
-	
+	for i in range(1000):
+		#_NNP.getNNP(Xc, nnl)
 		#nnl = np.array([get(i) for i in Xc])
-		pool_obj.map(execute,pool_indexes)
+		#pool_obj.map(execute,pool_indexes)
 		#nnl = Parallel(n_jobs=24)(delayed(process)(i) for i in range(pool_indexes))
-		b = Xc*(10*2j/ht + 1/(hx**2))-(Xn+Xp)/(2*hx**2) #+ Xc*nnl
+		b = Xc*(10*2j/ht + 1/(hx**2))-(Xn+Xp)/(2*hx**2)#+ Xc*nnl
 		b[0] = b[-1] = 0
 	
 		Xc = solve_banded((1, 1), ab, b)
@@ -57,7 +62,10 @@ for j in range(10):
 	
 	plt.plot(x, np.abs(Xc**2))
 	print(j)
+######################################################################################
 pool_obj.close()
+######################################################################################
+
 plt.plot(x, np.abs(X0**2))
 
 print(np.abs(np.sum(Xc))**2 - np.abs(np.sum(X0))**2)
--- nonlinearPart/nonlinearPart.py
+++ nonlinearPart/nonlinearPart.py
@@ -2,48 +2,74 @@ from scipy import integrate
 from numpy import sin, cos, exp, pi, sqrt
 from scipy.special import gamma, factorial
 from numba import njit, prange
+from joblib import Parallel, delayed
 import numpy as np
+import time
+import multiprocessing
 
-def get(U):
-    return GG(U)
 
-def GG(U):
-    mm = np.float64(7)
-    kk = np.float64(9)
-    W0 = np.float64(4.3e-12)
-    j = np.float64(1/10.6e-15)
-    _FF = np.zeros(int(mm*kk))
-    _A1 = np.zeros(int(mm*kk))
-    _Gp = np.zeros(int(mm*kk))
-    def gammaPart(q, U):
-        r = np.arange(5)
-        up = ((-1)**r)*(q**(2*r+1))
-        down = 2**(2*r+1)*gamma(r+1)*gamma(r+2)
-        return np.sum(up*np.absolute(U)**(2*r)/down)
+class NonlinearPart():
+	mm = np.float64(7)
+	kk = np.float64(9)
+	W0 = np.float64(4.3e-12)
+	j = np.float64(1/10.6e-15)
+	_FF = np.zeros(int(mm*kk))
+	_A1 = np.zeros(int(mm*kk))
+	_Gp = np.zeros(int(mm*kk))
+	_sumFF = np.zeros(int(mm*kk))
+	_cachedGammaUp = np.empty((int(kk), 5))
+	_cachedGammaDown = np.empty((5))
+	r = np.arange(5)
+	def getNNP(self, inArray, outArray):
+		
+		outArray = Parallel(n_jobs=512)(delayed(self.GG)(element) for element in inArray)
+		
+	def get(self, U):
+		start = time.time()
+		a = self.GG(U)
+		end = time.time()
+		print(end-start)
+		return a
+		#return self.GG(U)
 
-    def b(i):
-        return cos(pi*i/mm)
-    def ee(x, i): 
-        return W0 * sqrt(1.0 + 4.0*cos(x)*b(i)+4.0*b(i)*b(i))
-    def A1(i,k):
-        if _A1[int(i*kk + k)] == 0.0:
-            _A1[int(i*kk + k)] = integrate.quad(lambda x: ee(x, i)*cos(k*x), -pi, pi)[0]/pi
-        return _A1[int(i*kk + k)] 
-    def tetta(x,i,k):    
-        return exp(-(j*A1(i,0)/2+np.sum([j*A1(i,_k)*cos(_k*x) for _k in np.arange(1, kk)])))/(1+ exp(-(j*A1(i,0)/2+np.sum([j*A1(i,_k)*cos(_k*x) for _k in np.arange(1, kk)]))))
+	def gammaPart(self, q, U):
+        	if np.sum(self._cachedGammaUp[int(q)]):
+        		self._cachedGammaUp[int(q)] = ((-1)**self.r)*(q**(2*self.r+1))
+	        if np.sum(self._cachedGammaDown):
+        		self._cachedGammaDown = 2**(2*self.r+1)*gamma(self.r+1)*gamma(self.r+2)
+        	return np.sum(self._cachedGammaUp[int(q)]*np.absolute(U)**(2*self.r)/self._cachedGammaDown)
 
-    def FF(i,k):
-        def func(i,k):
-            return integrate.quad(lambda x :tetta(x,i,k)*cos(k*x),-pi,pi)[0]
-        def func2(i,k):    
-            return integrate.quad(lambda x :tetta(x,i,k),-pi,pi)[0]
-        if _FF[int(i*kk + k)] == 0.0:
-            _FF[int(i*kk + k)] = -k*A1(i,k)*func(i,k)/np.sum([func2(_i,k) for _i in np.arange(1, mm)])/W0
+	def b(self, i):
+        	return cos(pi*i/self.mm)
+	def ee(self,  x, i): 
+        	return self.W0 * sqrt(1.0 + 4.0*cos(x)*self.b(i)+4.0*self.b(i)*self.b(i))
+	def A1(self,  i,k):
+        	if self._A1[int(i*self.kk + k)] == 0.0:
+        	    self._A1[int(i*self.kk + k)] = integrate.quad(lambda x: self.ee(x, i)*cos(k*x), -pi, pi)[0]/pi
+        	return self._A1[int(i*self.kk + k)] 
+	def tetta(self, x,i,k):    
+        	return exp(-(self.j*self.A1(i,0)/2+np.sum([self.j*self.A1(i,_k)*cos(_k*x) for _k in np.arange(1, self.kk)])))/(1+ exp(-(self.j*self.A1(i,0)/2+np.sum([self.j*self.A1(i,_k)*cos(_k*x) for _k in np.arange(1, self.kk)]))))
 
-        return _FF[int(i*kk + k)]
-    _sum = 0
-    for _k in np.arange(kk):
-        _sum += gammaPart(_k, U)*np.sum([FF(_i,_k) for _i in np.arange(1, mm)])
-        #print(_k, "-", _sum)
-    return _sum
+	def FF(self, i,k):
+        	def func(i,k):
+            		return integrate.quad(lambda x :self.tetta(x,i,k)*cos(k*x),-pi,pi)[0]
+        	def func2(i,k):    
+            		return integrate.quad(lambda x :self.tetta(x,i,k),-pi,pi)[0]
+        	if self._FF[int(i*self.kk + k)] == 0.0:
+           		self._FF[int(i*self.kk + k)] = -k*self.A1(i,k)*func(i,k)/np.sum([func2(_i,k) for _i in np.arange(1, self.mm)])/self.W0
 
+	        return self._FF[int(i*self.kk + k)]
+
+	def GG(self, U):
+		_sum = 0
+		for _k in np.arange(self.kk):
+			if self._sumFF[int(_k)] == 0.0:
+				self._sumFF[int(_k)] = np.sum([self.FF(_i,_k) for _i in np.arange(1, self.mm)])
+			_sum += self.gammaPart(_k, U)*self._sumFF[int(_k)]
+			#print(_k, "-", self._sumFF[int(_k)])
+		#print("+")
+		return _sum
+
+#a = NonlinearPart()
+#for i in range(1000):
+	#print(a.get(0.001*(i+i*1j)))
