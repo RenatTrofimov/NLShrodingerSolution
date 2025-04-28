81bcaf1a36a8f4ba082204ce26edc97c5ef1202f
Renat Trofimov <renatrofimov@gmail.com>
2025-04-28 14:26:00 +0300

    stable-ds-addon

--- main.py
+++ main.py
@@ -6,123 +6,139 @@ from scipy.special import gamma
 from joblib import Parallel, delayed
 import multiprocessing
 import time
-
+import scipy
 import numpy as np
 import matplotlib.pyplot as plt
 from math import factorial, sqrt, pi, cos, exp
 
+import pdb
+
+import mpmath
+import numpy as np
+import matplotlib.pyplot as plt
+from math import factorial, pi, cos
+
 def Besse_CNT():
+    # Инициализация mpmath с высокой точностью
+    mpmath.mp.dps = 10  # Количество значащих цифр
+    
+    # Конвертация констант в mpmath формате
+    def mpf(x):
+        return mpmath.mpf(str(x))
+    
     # Simulation parameters
-    M = 1000
-    tEnd = 2
-    xEnd = 10
-    dt = 0.001
-    dx = 2 * xEnd / M
-    r = dt / (2 * dx * dx)
-    lamda = 1
+    M = 2000
+    tEnd = mpf(2)
+    xEnd = mpf(20)
+    dt = mpf('0.001')
+    dx = mpf(2)*xEnd/mpf(M)
+    r = dt/(mpf(2)*dx**2)
+    lamda = mpf(1)
     nn = 1
-    Nt = int(tEnd / dt)
-    jj = 1j  # imaginary unit in Python
+    Nt = tEnd/dt
+    jj = mpmath.mpc(1j)  # мнимая единица
 
     # Physical constants
-    el = -4.8e-10
-    h1 = 1.055e-27
-    k_B = 1.38e-16  # Boltzmann constant (CGS)
+    el = mpf('-4.8e-10')
+    h1 = mpf('1.055e-27')
+    k_B = mpf('1.38e-16')  # Boltzmann constant (CGS)
     
     # CNT Constants and parameters
-    gamma0 = 2.7 * 1.6e-12
-    b_CNT = 0.142e-7
-    a_CNT = (3/2) * b_CNT
+    gamma0 = mpf('2.7') * mpf('1.6e-12')
+    b_CNT = mpf('0.142e-7')
+    a_CNT = (mpf(3)/mpf(2)) * b_CNT
     m = 7
-    T = 77  # Temperature
-    rel_perm = 4
-    el_concentration = 1e18
-    omega0 = 2 * abs(el) * a_CNT * sqrt(pi * el_concentration * gamma0) / h1
+    T = mpf(77)  # Temperature
+    rel_perm = mpf(4)
+    el_concentration = mpf('1e18')
+    omega0 = mpf(2)*mpmath.fabs(el)*a_CNT*mpmath.sqrt(pi*el_concentration*gamma0)/h1
     
-    omega = 4e14
-    kappa = 2 * sqrt(rel_perm) * omega / omega0  # wave vector
+    # Beam wave-vector
+    omega = mpf('4e14')
+    kappa = mpf(2)*mpmath.sqrt(rel_perm)*omega/omega0
     l_max = 5
     
+    # Energy spectrum function
     def energy_spectrum(ksi, s):
-        return gamma0 * sqrt(1 + 4 * cos(ksi) * cos(pi * s / m) + 4 * cos(pi * s / m)**2)
+        return gamma0 * mpmath.sqrt(1 + 4*mpmath.cos(ksi)*mpmath.cos(pi*s/mpf(m)) + 
+                                  4*mpmath.cos(pi*s/mpf(m))**2)
     
     N_garm = 9
-    g = 0.25
-    E0 = 0.10e7  # V/cm
-    E0 = 1 * (E0) / 300  # SGS(E)
-    A0 = E0 * abs(el) * a_CNT / (h1 * omega)
+    g = mpf('0.25')
+    E0 = mpf('0.10e7')  # V/cm
+    E0 = mpf(1)*(E0)/mpf(300)
+    A0 = E0*mpmath.fabs(el)*a_CNT/(h1*omega)
     
-    Integral_1 = np.zeros((N_garm, m))
-    delta = np.zeros((N_garm, m))
-    delta_relative = np.zeros((N_garm, m))
-    Integral_1_0 = np.zeros(m)
-    delta_0 = np.zeros(m)
-    delta_0_relative = np.zeros(m)
-    Integral_2 = np.zeros((N_garm, m))
-    Summa_v_integralah = np.zeros(m)
-    Integral_3 = np.zeros(m)
-    F = np.zeros((N_garm, m))
-    G = np.zeros(N_garm)
+    # Initialize arrays
+    Integral_1 = mpmath.matrix(N_garm, m)
+    delta = mpmath.matrix(N_garm, m)
+    delta_relative = mpmath.matrix(N_garm, m)
+    Integral_1_0 = mpmath.matrix(1, m)
+    delta_0 = mpmath.matrix(1, m)
+    delta_0_relative = mpmath.matrix(1, m)
+    Integral_2 = mpmath.matrix(N_garm, m)
+    Summa_v_integralah = mpmath.matrix(1, m)
+    Integral_3 = mpmath.matrix(m, 1)
+    F = mpmath.matrix(N_garm, m)
+    G = mpmath.matrix(N_garm, 1)
     
-    steps = 10000
+    steps = 100
     
-    # Integral_1[r,s]
+    # Integral calculations with mpmath
     for r in range(N_garm):
         for s in range(m):
-            Integral_1[r, s] = 0
+            Integral_1[r,s] = mpf(0)
             for k in range(1, 2*steps + 1):
-                p = -pi + (pi/steps) * k
-                Integral_1[r, s] += 1 * (pi/steps) * energy_spectrum(p, s) * cos((r+1) * p)
+                p = -mpmath.pi + (mpmath.pi/steps)*k
+                Integral_1[r,s] += (mpmath.pi/steps)*energy_spectrum(p,s)*mpmath.cos((r+1)*p)
             
-            # Coefficients in the Fourier expansion: delta[r,s]
-            delta[r, s] = (1/pi) * Integral_1[r, s]
-            delta_relative[r, s] = delta[r, s] / gamma0
+            delta[r,s] = (1/mpmath.pi)*Integral_1[r,s]
+            delta_relative[r,s] = delta[r,s]/gamma0
     
-    # Integral_1[0,s], when r = 0
     for s in range(m):
-        Integral_1_0[s] = 0
+        Integral_1_0[0,s] = mpf(0)
         for k in range(1, 2*steps + 1):
-            p = -pi + (pi/steps) * k
-            Integral_1_0[s] += 1 * (pi/steps) * energy_spectrum(p, s) * cos(0 * p)
+            p = -mpmath.pi + (mpmath.pi/steps)*k
+            Integral_1_0[0,s] += (mpmath.pi/steps)*energy_spectrum(p,s)
         
-        delta_0[s] = (1/pi) * Integral_1_0[s]
-        delta_0_relative[s] = delta_0[s] / gamma0
+        delta_0[0,s] = (1/mpmath.pi)*Integral_1_0[0,s]
+        delta_0_relative[0,s] = delta_0[0,s]/gamma0
     
-    # Integral_2[r,s]
     for r in range(N_garm):
         for s in range(m):
-            Integral_2[r, s] = 0
+            Integral_2[r,s] = mpf(0)
             for k in range(1, 2*steps + 1):
-                p = -pi + (pi/steps) * k
-                Summa_v_integralah[s] = 0
+                p = -mpmath.pi + (mpmath.pi/steps)*k
+                Summa_v_integralah[0,s] = mpf(0)
                 for r2 in range(N_garm):
-                    Summa_v_integralah[s] += (delta[r2, s]/(k_B*T)) * cos((r2+1)*p)
+                    Summa_v_integralah[0,s] += (delta[r2,s]/(k_B*T))*mpmath.cos((r2+1)*p)
                 
-                Integral_2[r, s] += 1 * (pi/steps) * cos((r+1)*p) / (1 + exp((delta_0[s]/(2*k_B*T)) + Summa_v_integralah[s]))
-    
-    # Integral_3[r,s]
+                exponent = (delta_0[0,s]/(2*k_B*T)) + Summa_v_integralah[0,s]
+                term = 1 + mpmath.exp(exponent)
+                Integral_2[r,s] += (mpmath.pi/steps)*mpmath.cos((r+1)*p)/term
+    print( "i2" )
     for s in range(m):
-        Integral_3[s] = 0
+        Integral_3[s,0] = mpf(0)
         for k in range(1, 2*steps + 1):
-            p = -pi + (pi/steps) * k
-            Summa_v_integralah[s] = 0
+            p = -mpmath.pi + (mpmath.pi/steps)*k
+            Summa_v_integralah[0,s] = mpf(0)
             for r2 in range(N_garm):
-                Summa_v_integralah[s] += (delta[r2, s]/(k_B*T)) * cos((r2+1)*p)
+                Summa_v_integralah[0,s] += (delta[r2,s]/(k_B*T))*mpmath.cos((r2+1)*p)
             
-            Integral_3[s] += 1 * (pi/steps) / (1 + exp((delta_0[s]/(2*k_B*T)) + Summa_v_integralah[s]))
+            exponent = (delta_0[0,s]/(2*k_B*T)) + Summa_v_integralah[0,s]
+            term = 1 + mpmath.exp(exponent)
+            Integral_3[s,0] += (mpmath.pi/steps)/term
     
-    # Summa_v_znamenatele
-    Summa_v_znamenatele = np.sum(Integral_3)
-    
-    # F[r,s]
+    Summa_v_znamenatele = mpmath.fsum(Integral_3[:,0])
+    print( "i3" )
     for r in range(N_garm):
         for s in range(m):
-            F[r, s] = - (r+1) * (delta[r, s]/gamma0) * (Integral_2[r, s]/Summa_v_znamenatele)
-    
-    # G[r]
+            F[r,s] = -(r+1)*(delta[r,s]/gamma0)*(Integral_2[r,s]/Summa_v_znamenatele)
+    print( "F[r,0]" )
     for r in range(N_garm):
-        G[r] = np.sum(F[r, :])
-    
+        G[r,0] = mpmath.fsum(F[r,:])
+    print( "G[r,0]" )
+    # Initialize arrays for simulation
     x = np.zeros(M)
     Resh = np.zeros(M)
     U0 = np.zeros(M, dtype=complex)
@@ -135,10 +151,12 @@ def Besse_CNT():
     A_plus = np.zeros((M, M), dtype=complex)
     A_minus = np.zeros((M, M), dtype=complex)
     
+     # Initial conditions
     for i in range(M):
         x[i] = 0 + dx * (i+1)
         U0[i] = A0 * exp(-(x[i] - xEnd)**2 / g)
     
+    # Setup A_plus and A_minus matrices
     for i in range(1, M):
         A_plus[i, i-1] = 1
         A_minus[i, i-1] = -1
@@ -152,10 +170,14 @@ def Besse_CNT():
     A_plus[M-1, 0] = 1
     A_minus[M-1, 0] = -1
     
-    Coeff = np.zeros(l_max)
+    # Coefficients for nonlinear term
+    Coeff = [mpf(0) for _ in range(l_max)]
     for l in range(l_max):
-        Coeff[l] = ((-1)**(l+1)) * (l+1) / (factorial(l+1) * factorial(l+2) * (2**(2*(l+1))))
-    
+        Coeff[l] = ((-1)**(l+1))*(l+1)/(mpmath.factorial(l+1)*mpmath.factorial(l+2)*(2**(2*(l+1))))
+    Coeff = np.array([float(str(i)) for i in Coeff ])
+	
+    G = np.array([float(str(i)) for i in G ])
+    # Main simulation loop
     while nn < Nt:
         for i in range(M):
             V0[i] = abs(U0[i])**2
@@ -170,13 +192,14 @@ def Besse_CNT():
             alfa_minus[i] = -2 * kappa * jj / r - 2 - dx**2 * nonlin[i]
             A_plus[i, i] = alfa_plus[i]
             A_minus[i, i] = -alfa_minus[i]
-        
+        print( nn )
         B = np.dot(A_minus, U0)
-        U1 = np.linalg.solve(A_plus, B)
+        U1 = scipy.linalg.solve(A_plus, B)
         
         U0 = U1.copy()
         nn += 1
     
+    # Convert results to float for plotting
     plt.plot(x, abs(U1)/A0)
     plt.show()
 
@@ -277,6 +300,7 @@ class NNs_NonlinearPart():
 		#end
 		#% -------------------------------------------------------------------------
 		#% Integral_3[r,s]: --------------------------------------------------------
+		pdb.set_trace()
 		for s in np.arange( 1, self.m):
 			self.Integral_3[s] = 0
 			for k in np.arange( 1 , 2*self.steps):
